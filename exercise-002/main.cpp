0aef132ce1f3d91acbd644d21895b83670223b70
lronman00 <157050273+lronman00@users.noreply.github.com>
2025-11-14 13:59:39 +0100

    Add garage door controller implementation

--- exercise-002/main.cpp
+++ exercise-002/main.cpp
@@ -1,83 +1,25 @@
-#include <fmt/chrono.h>
 #include <fmt/format.h>
-
-/* .bss Segment */
-int foo;
-int foo2 = 0;
-
-/* .data Segment */
-int data = 45;
-int data2 = 43;
-
-/* .rodata Segment */
-const int rodata = 45;
-const int rodata2 = 43;
+#include "graveyard.h"
 
 
+// Eigene globale Variable im Hauptmodul
+int undead = 7;
 
-void doSomething(int value)
-{
-    value = value + 1;
-    fmt::print("doSomething, value {}\n", value);
-}
+int main() {
+    fmt::println("🪦 Globale Variable sagt: {} (addr: {})", undead, fmt::ptr(&undead));
 
-void doSomethingPtr(int* ptr)
-{
-    *ptr = *ptr + 1;
-    fmt::print("doSomethingPtr, value {}\n", *ptr);
-}
+    awaken();       // lokale "undead" (Stack)
+    persistent();   // statische lokale "undead" (Data)
+    persistent();   // bleibt erhöht
 
-int* doReturnAPtr()
-{
-    int value = 4711;
-    fmt::print("doReturnAPtr, value {}\n", value);
-    return &value;
-}
+    fmt::println("🪦 Globale Variable nach Aufruf: {} (addr: {})",
+                 undead, fmt::ptr(&undead));
 
-void doSomethingStatic()
-{
-    static int value = 56;
-    value = value + 1;
-    fmt::print("doSomethingStatic, value {}\n", value);
-}
-auto main(int argc, char** argv) -> int
-{
-    /**
-     * The {fmt} lib is a cross platform library for printing and formatting text
-     * it is much more convenient than std::cout and printf
-     * More info at https://fmt.dev/latest/api.html
-     */
-    fmt::print("Hello, {}!\n", argv[0]);
+    // Bonus
+    curse();
 
-    doSomething(42);
-    int bar = 42;
-    doSomething(bar);
-    fmt::print("The value of bar is: {}\n", bar);
-
-    doSomethingPtr(&bar);
-    fmt::print("The value of bar after doSomethingPtr is: {}\n", bar);
-
-    int* returnedPtr = doReturnAPtr();
-    fmt::print("The value of returnedPtr content  after doReturnAPtr is: {}\n", *returnedPtr);
-    doSomething(42);
-    fmt::print("The value of returnedPtr content  after doReturnAPtr is: {}\n", *returnedPtr);
-
-    doSomethingStatic();
-    doSomethingStatic();
-    doSomethingStatic();
-    doSomethingStatic();
-
-    fmt::print("Value of foo {} address of foo {}\n", foo, fmt::ptr(&foo));
-    fmt::print("Value of foo2 {} address of foo2 {}\n", foo2, fmt::ptr(&foo2));
-    fmt::print("Value of data {} address of data {}\n", data, fmt::ptr(&data));
-    fmt::print("Value of data2 {} address of data2 {}\n", data2, fmt::ptr(&data2));
-    fmt::print("Value of rodata {} address of rodata {}\n", rodata, fmt::ptr(&rodata));
-    fmt::print("Value of rodata2 {} address of rodata2 {}\n", rodata2, fmt::ptr(&rodata2));
-
-    int var;
-    static int var2;
-    fmt::print("Value of var {} address of var {}\n", var, fmt::ptr(&var));
-    fmt::print("Value of var2 {} address of var2 {}\n", var2, fmt::ptr(&var2));
-
-    return 0; /* exit gracefully*/
+    // Adressen der globalen Variablen aus verschiedenen TUs kann man
+    // nicht direkt alle hier auslesen, aber man sieht unterschiedliche
+    // Werte & Adressen in den jeweiligen Funktionen.
+    return 0;
 }
--- exercise-003/main.cpp
+++ exercise-003/main.cpp
@@ -1,15 +1,131 @@
 #include <fmt/chrono.h>
 #include <fmt/format.h>
+#include <iostream>
+#include <string>
+
+enum class GaragenState {
+    ZU,
+    AUF,
+    HOCHFAHREND,
+    RUNTERFAHREND,
+    GESTOPPT
+};
+
+class Garagentor {
+private:
+    GaragenState currentState;
+    bool endschalterOben;
+    bool endschalterUnten;
+    bool tasteGedrueckt;
+
+public:
+    Garagentor() : currentState(GaragenState::ZU), 
+                   endschalterOben(false), 
+                   endschalterUnten(true),
+                   tasteGedrueckt(false) {}
+
+    void setEndschalterOben(bool status) {
+        endschalterOben = status;
+        updateState();
+    }
+
+    void setEndschalterUnten(bool status) {
+        endschalterUnten = status;
+        updateState();
+    }
+
+    void setTasteGedrueckt(bool gedrueckt) {
+        tasteGedrueckt = gedrueckt;
+        if (gedrueckt) {
+            handleTaste();
+        }
+    }
+
+private:
+    void handleTaste() {
+        switch (currentState) {
+            case GaragenState::ZU:
+                fmt::print("Taste gedrückt - Fahre hoch\n");
+                currentState = GaragenState::HOCHFAHREND;
+                break;
+                
+            case GaragenState::AUF:
+                fmt::print("Taste gedrückt - Fahre runter\n");
+                currentState = GaragenState::RUNTERFAHREND;
+                break;
+                
+            case GaragenState::HOCHFAHREND:
+            case GaragenState::RUNTERFAHREND:
+                fmt::print("Taste gedrückt - Stop\n");
+                currentState = GaragenState::GESTOPPT;
+                break;
+                
+            case GaragenState::GESTOPPT:
+                fmt::print("Taste gedrückt - Fahre hoch\n");
+                currentState = GaragenState::HOCHFAHREND;
+                break;
+        }
+    }
+
+    void updateState() {
+        if (currentState == GaragenState::HOCHFAHREND && endschalterOben) {
+            fmt::print("Endschalter oben erreicht - Tor offen\n");
+            currentState = GaragenState::AUF;
+        }
+        else if (currentState == GaragenState::RUNTERFAHREND && endschalterUnten) {
+            fmt::print("Endschalter unten erreicht - Tor zu\n");
+            currentState = GaragenState::ZU;
+        }
+    }
+
+public:
+    void printStatus() {
+        std::string stateStr;
+        switch (currentState) {
+            case GaragenState::ZU: stateStr = "ZU"; break;
+            case GaragenState::AUF: stateStr = "AUF"; break;
+            case GaragenState::HOCHFAHREND: stateStr = "HOCHFAHREND"; break;
+            case GaragenState::RUNTERFAHREND: stateStr = "RUNTERFAHREND"; break;
+            case GaragenState::GESTOPPT: stateStr = "GESTOPPT"; break;
+        }
+        
+        fmt::print("Status: {}, Endschalter Oben: {}, Unten: {}\n", 
+                   stateStr, endschalterOben, endschalterUnten);
+    }
+};
 
 auto main(int argc, char** argv) -> int
 {
-    /**
-     * The {fmt} lib is a cross platform library for printing and formatting text
-     * it is much more convenient than std::cout and printf
-     * More info at https://fmt.dev/latest/api.html
-     */
-    fmt::print("Hello, {}!\n", argv[0]);
+    fmt::print("Garagentor Steuerung - {}\n", argv[0]);
+    
+    Garagentor tor;
+    
+    // Testablauf
+    tor.printStatus();
+    
+    // Taste drücken um hochzufahren
+    tor.setTasteGedrueckt(true);
+    tor.printStatus();
+    
+    // Endschalter oben erreicht
+    tor.setEndschalterOben(true);
+    tor.printStatus();
+    
+    // Taste drücken um runterzufahren
+    tor.setTasteGedrueckt(true);
+    tor.printStatus();
+    
+    // Endschalter unten erreicht
+    tor.setEndschalterUnten(true);
+    tor.printStatus();
+    
+    // Während der Fahrt stoppen
+    tor.setTasteGedrueckt(true); // Hochfahren
+    tor.printStatus();
+    tor.setTasteGedrueckt(true); // Stoppen
+    tor.printStatus();
+    tor.setTasteGedrueckt(true); // Weiterfahren
+    tor.printStatus();
 
-
-    return 0; /* exit gracefully*/
+    return 0;
 }
